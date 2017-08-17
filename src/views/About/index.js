cc653e2b53211e219fea157b6408129780413a37
alisa <alisa@dxt.cn>
2017-08-17 17:14:16 +0800

    路由测试

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
-
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom'
+import Main from './Main'
 
 export default class About extends Component {
     render() {
         return (
+          <router>
             <span>我是一个大好人</span>
+            <ul>
+              <li><Link to="/main">tag</Link></li>
+            </ul>
+             <Route path="/main" component={Main}></Route>
+          </router>
+
         )
     }
 }
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -12,6 +12,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <a href="/about">about</a>
+          <br/>
+          <a href="/tag">tag</a>
         </p>
       </div>
     );
